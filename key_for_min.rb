0ed36d508607dae6c8f39e8e2465c0b930b6cea0
Ryna Truong <rynatruong@outlook.com>
2020-02-23 19:52:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest = 
   name_hash.collect do |key, value|
     if value < key[value]
     puts key
